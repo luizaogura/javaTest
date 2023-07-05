85f9e391b68b46a9d3e3b002dd5548a98ad71a81
Luiza Ogura <110496349+luizaogura@users.noreply.github.com>
2023-07-05 18:10:50 -0300

    Add files via upload

---
+++ exercises/1.java
@@ -0,0 +1,16 @@
+public class ObjetosString {
+
+    public static void main(final String[] args) {
+
+        String eu = "Eu";
+        String ja = " já";
+        String sei = " sei";
+        String usar = " usar";
+        String strings = " Strings";
+        String em = " em";
+        String java = " Java";
+
+        String frase = eu + ja + sei + usar + strings + em + java;
+        System.out.println(frase); // Eu já sei usar Strings em Java
+    }
+}
---
+++ exercises/src/exercises/javaTest.java
@@ -0,0 +1,11 @@
+package exercises;
+
+public class javaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá mundo");
+
+	}
+
+}
